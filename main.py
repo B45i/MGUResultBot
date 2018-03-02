3c5bf6e9c37a7d75467c73ef7ecbf308114d982a
b45i <amalshajan2011@gmail.com>
2018-03-02 20:13:44 +0530

    cheking page at an interval

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 from config import token, chat_id
 
 
 TELEGRAM_API = 'https://api.telegram.org/bot' + token
 MGU_URL = 'http://projects.mgu.ac.in/bTech/btechresult/index.php?module=public&attrib=result&page=result'
-
+interval = 10 # Time interval in minutes
 
 def send_message(text):
     data = {'chat_id': chat_id, 'text': text}
@@ -14,24 +15,26 @@ def send_message(text):
     # print response.content
 
 def check_result():
-    page = requests.get(MGU_URL)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    exams = str(soup.findAll('option'))
+    while(True):
+        page = requests.get(MGU_URL)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        exams = str(soup.findAll('option'))
 
-    prev_file = open('previous.txt', 'r')
-    prev_exams = prev_file.read()
-    prev_file.close()
-    
-    if prev_exams != exams:
-        msg_str = 'Results for :\n'
-        for list in soup.select('option')[1:]:
-            msg_str += list.contents[0] + '\n'
-        msg_str += 'are now avilable on ' + MGU_URL
-        send_message(msg_str)
+        prev_file = open('previous.txt', 'r')
+        prev_exams = prev_file.read()
+        prev_file.close()
+        
+        if prev_exams != exams:
+            msg_str = 'Results for :\n'
+            for list in soup.findAll('option')[1:]:
+                msg_str += list.contents[0] + ',\n'
+            msg_str += 'are now avilable on ' + MGU_URL
+            send_message(msg_str)
 
-        file = open('previous.txt', 'w')
-        file.write(exams)
-        file.close()
+            file = open('previous.txt', 'w')
+            file.write(exams)
+            file.close()
+        time.sleep(interval*60)
 
 def main():
     check_result()
